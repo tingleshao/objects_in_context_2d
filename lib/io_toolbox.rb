41d332bf7bb2486b967d8a9a4bbb396e17b6b9d0
my ubuntu <tingleshao@gmail.com>
2013-03-18 09:42:49 -0400

    fix xml write bug 0

--- lib/io_toolbox.rb
+++ lib/io_toolbox.rb
@@ -115,13 +115,8 @@ end
 
 
 def saveSrepDataAsXML(file_name,sreps,number_of_sreps)
-	
-	atoms_lst.each_with_index do |atom, ind|
-	    atom_xy_lst << [atom.x, atom.y]
-	    spoke_length_lst << atom.spoke_length
-	    spoke_dir_lst << atom.spoke_direction
-	end
-	builder = Nokogiri::XML::Builder.new do |data|
+
+	builder = Nokogiri::XML::Builder.new do |xml|
 	   xml.data {
 	        xml.number_of_sreps  number_of_sreps.to_s
 	        xml.sreps {
@@ -131,42 +126,48 @@ def saveSrepDataAsXML(file_name,sreps,number_of_sreps)
 			   atom_xy_lst = []
 			   spoke_length_lst = []
 		       	   spoke_dir_lst = []
-			   xml.srep{
-			       xml.number_of_base_points  len(srep.atoms).to_s
+			   	
+                       	   atoms_lst.each_with_index do |atom, ind|
+	                      atom_xy_lst << [atom.x, atom.y]
+	                      spoke_length_lst << atom.spoke_length
+	    		      spoke_dir_lst << atom.spoke_direction
+	    		   end
+			   xml.srep('xmlns' => ('srep'+i.to_s)){
+			       xml.number_of_base_points  srep.atoms.length.to_s
                                xml.points {
-                                   atoms_lst.length.times do |i| 
+                                   atoms_lst.length.times do |j| 
 				       	   xml.point {
-						xml.x atom_xy_lst[i][0]
-						xml.y atom_xy_lst[i][1]
+						xml.x atom_xy_lst[j][0].to_i
+						xml.y atom_xy_lst[j][1].to_i
 		                           }
 			           end
 			       }
                                xml.radius {
-                                   atoms_lst.length.times do |i| 
+                                   atoms_lst.length.times do |j| 
                                            xml.disk {
-                                               xml.r spoke_length_lst[i]
-                                               xml.r spoke_length_lst[i]
-                                               if i ==0 or i==atoms_lst.length - 1
-                                                  xml.r spoke_length_lst[i]
+                                               xml.r spoke_length_lst[j][0].to_i
+                                               xml.r spoke_length_lst[j][0].to_i
+                                               if j ==0 or j==atoms_lst.length - 1
+                                                  xml.r spoke_length_lst[i][0].to_i
                                                end
                                            }
                                    end
                                }
                                xml.spoke_dirs {
-                                    atoms_lst.length.times do |i| 
+                                    atoms_lst.length.times do |j| 
                                              xml.atom {
                                                 xml.spoke {
-                                                   xml.x spoke_dir_lst[i][0][0]
-                                                   xml.y spoke_dir_lst[i][0][1]
+                                                   xml.x spoke_dir_lst[j][0][0]
+                                                   xml.y spoke_dir_lst[j][0][1]
                                                 }
                                                 xml.spoke {
-                                                   xml.x spoke_dir_lst[i][1][0]
-                                                   xml.y spoke_dir_lst[i][1][1]
+                                                   xml.x spoke_dir_lst[j][1][0]
+                                                   xml.y spoke_dir_lst[j][1][1]
                                                 }
-                                                if i == 0 or i == atoms_lst.length - 1
+                                                if j == 0 or j == atoms_lst.length - 1
                                                    xml.spoke {
-                                                     xml.x spoke_dir_lst[i][2][0]
-					             xml.y spoke_dir_lst[i][2][1]
+                                                     xml.x spoke_dir_lst[j][2][0]
+					             xml.y spoke_dir_lst[j][2][1]
                                                    }
                                                 end
                                              }
@@ -177,37 +178,10 @@ def saveSrepDataAsXML(file_name,sreps,number_of_sreps)
 		}		
 	   }
 	end	
-	f = file.open(file_name,'w')
+	f = File.open(file_name,'w')
         f.write(builder.to_xml)
 	f.close
 end
 
 
 
-=begin
-  points0 = [[110,100],[160,75],[210,50],[260,60],[310,80]]
-  l0 = [[35,35,35],[40,40],[30,30],[40,40],[35,35,35]]
-  u0 = [[[-1,3],[-0.1,-4],[-9,1]],[[-1,4],[1.1,-3]],[[-1,4],[0.2,-6]],[[1,9],[0.05,-8]],[[1,2],[1,-5],[6,1]]]
-  srep0 = generate2DDiscreteSrep(points0,l0,u0,0.01,0)
-  srep0.orientation = [0,1]
-  $sreps = [srep0]
-
-  points1 = [[200,190],[250,190],[300,200],[350,180],[400,160]]
-  l1 = [[35,35,35],[40,40],[45,45],[40,40],[35,35,35]]
-  u1 = [[[-1,6],[0.5,-3],[-9,1]],[[-1,4],[-1,-3]],[[-1,4],[-0.1,-6]],[[1,9],[1,-1.5]],[[1,2],[2,-5],[6,1]]]
-  srep1 = generate2DDiscreteSrep(points1,l1,u1,0.01,1)
-  srep1.color = Color.green
-  srep1.orientation = [0,1]
-  $sreps << srep1
-
-  points2 = [[30,50],[10,100],[9,150],[20,200],[50,240],[110,290]]
-  l2 = [[35,35,35],[35,35],[40,40],[35,35],[40,40],[40,40,40]]
-  u2 = [[[6,1],[6,-0.5],[-9,1]],[[-1,4],[3,-0.5]],[[-1,4],[5,-0.5]],[[1,9],[5,1]],[[1,9],[5,3]],[[1,2],[3,5],[6,1]]]
-  srep2 = generate2DDiscreteSrep(points2,l2,u2,0.01,2)
-  srep2.color = Color.purple
-  srep2.orientation = [1,0]
-  $sreps << srep2
-  
-  refresh @points, $sreps, @shifts
-=end
-    
