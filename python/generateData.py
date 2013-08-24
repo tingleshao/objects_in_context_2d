a41180da77ebb67e011a99409a1c2acf3ad41b05
my ubuntu <tingleshao@gmail.com>
2013-08-24 15:06:31 -0400

    update on the data generation code

---
+++ python/generateData.py
@@ -0,0 +1,20 @@
+# this script is for generating base pts x y's and (probabily) radius' for test statistics
+# two major modes of variations are:
+# 	1. srep0, horizontal stretching 
+#	2. srep1, left side raising / dropping
+# other places has small variations sampled from gaussian dist.
+
+
+from numpy import *
+from scipy import *
+
+import sys
+
+print "hello"
+
+# argument 1: how many sample data we need 
+# argument 2: noise gaussian variance
+
+# note that the variance for two major modes of variations are hard coded into the code
+# this may be changed later
+
