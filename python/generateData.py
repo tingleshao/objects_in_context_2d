d4be74cd70d168668daba75ca31d3e934fc69403
my ubuntu <tingleshao@gmail.com>
2013-08-25 13:49:14 -0400

    add noise

--- python/generateData.py
+++ python/generateData.py
@@ -5,7 +5,7 @@
 # other places has small variations sampled from gaussian dist.
 
 
-from numpy import *
+import numpy as np
 from scipy import *
 
 import sys
@@ -25,7 +25,11 @@ print "hello"
 
 num_of_data = int(sys.argv[1])
 gaussian_variance = float(sys.argv[2])
+# gaussian seed
+s = np.random.normal(0,gaussian_variance,28)
+srep1_pts = [[90+s[0],100+s[1]],[150+s[2],75+s[3]],[210+s[4],50+s[5]],[270+s[6],60+s[7]],[330+s[8],80+s[9]]]
+srep2_pts = [[200+s[10],190+s[11]],[250+s[12],190+s[13]],[300+s[14],200+s[15]],[350+s[16],180+s[17]],[400+s[18],160+s[19]]]
+srep3_pts = [[-10+s[20],140+s[21]],[40+s[22],170+s[23]],[75+s[24],185+s[25]],[110+s[26],195+s[27]]
 
-srep1_pts = [[90,100],[150,75],[210,50],[270,60],[330,80]]
-srep2_pts = [[200,190],[250,190],[300,200],[350,180],[400,160]]
-srep3_pts = [[-10,140],[40,170],[75,185],[110,195]]
+
+
