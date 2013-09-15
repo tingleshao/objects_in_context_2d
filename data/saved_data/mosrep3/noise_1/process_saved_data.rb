04a0446e184dcc0cca8a751b5c8818a15bf34fb1
my ubuntu <tingleshao@gmail.com>
2013-09-15 15:07:57 -0400

    update IO code

--- data/saved_data/mosrep3/noise_1/process_saved_data.rb
+++ data/saved_data/mosrep3/noise_1/process_saved_data.rb
@@ -5,5 +5,34 @@
 $a_big_number = 100
 $end_disk_spoke_number = 20
 
+$dir = '' 
+
+
+# read interpolated spoke begin and spoke end data
+# 1. spoke begin data is for the single srep spoke length data, which should be computed with spoke end data.
+# 2. spoke end data is for computing the interpolated extended spoke length. 
+# read data
+interp_spoke_0_read = File.open($dir+'interpolated_spokes_0','r')
+interp_spoke_0_str = ''
+while line = interp_spoke_0_read.gets
+ 	interp_spoke_0_str += line	
+end
+interp_spoke_0_read.close
+#puts interp_spoke_0_str
+
+interp_spokes_begin_0_str = interp_spoke_0_str.split("========\n")[0]
+interp_spokes_end_0_str = interp_spoke_0_str.split("========\n")[1]
+#puts interp_spokes_begin_0_str
+
+#puts interp_spokes_end_0_str
+
+interp_spokes_begin_0_lst = interp_spokes_begin_0_str.strip[3..-3].split('], [')
+interp_spokes_begin_0_lst.each do |s|
+    puts s 
+end
+
+
+# Do point 2:
+
 
 
