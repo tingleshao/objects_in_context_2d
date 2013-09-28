02bfe103b0db6d5613778c4677dcf3d773c00419
my ubuntu <tingleshao@gmail.com>
2013-09-27 20:39:29 -0400

    37

---
+++ data/saved_data/mosrep3/noise_38/make_data_vector.rb
@@ -0,0 +1,47 @@
+# this script reads 8 11 files and construct a long data vector for analysis!
+# Author: Chong Shao / cshao@cs.unc.edu
+# 2013 all rights reserved.
+
+data_vector = File.open('data_vector.txt','w')
+
+3.times do |i|
+   reader = File.open('skeletal_curve_srep_' + i.to_s + '.txt', 'r')
+   while line = reader.gets
+      line.strip.split(' ').each do |e|
+         data_vector.puts  e
+      end
+   end
+   reader.close
+end
+
+3.times do |i|
+  reader = File.open('spoke_dir_srep_' + i.to_s + '.txt', 'r')
+  while line = reader.gets
+       line.strip.split(' ').each do |e|
+         data_vector.puts  e
+      end
+  end  
+  reader.close
+end
+
+3.times do |i|
+   reader = File.open('log_spoke_len_srep_' + i.to_s + '.txt', 'r')
+   while line = reader.gets
+       data_vector.puts line
+   end
+   reader.close
+end
+
+reader = File.open('extended_length.txt')
+while line = reader.gets
+    data_vector.puts line
+end
+reader.close
+
+reader = File.open('linked_spoke_index_in_other_obj.txt')
+while line = reader.gets
+    data_vector.puts line
+end
+reader.close
+puts "write data vector finished."
+data_vector.close
